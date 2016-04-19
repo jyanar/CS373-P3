2bf417a3a01b292765544695bfa731772a9d5b21
Michael Gibbons <michaelgibs123@gmail.com>
2016-04-18 21:33:01 -0500

    added observer implementation to the main

--- src/main/java/cs373/p3/Main.java
+++ src/main/java/cs373/p3/Main.java
@@ -1,7 +1,23 @@
 package cs373.p3;
 
+import cs373.p3.observer.Chinese_Chef;
+import cs373.p3.observer.French_Chef;
+import cs373.p3.observer.Mexican_Chef;
+import cs373.p3.observer.Restaurant;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        
+        Restaurant restaurant = new Restaurant();
+		restaurant.addObserver(new French_Chef());
+		restaurant.addObserver(new Chinese_Chef());
+		restaurant.addObserver(new Mexican_Chef());
+		
+		restaurant.setOrder(10);
+		restaurant.notifyObservers();
+		
+		restaurant.setOrder(20);
+		restaurant.notifyObservers();
     }
 }
